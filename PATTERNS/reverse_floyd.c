e028261a676dc599c19b9a0a1c4662f9512c1d12
Tagore <tagore1066@gmail.com>
2026-02-25 10:35:15 +0530

    patterens

--- PATTERNS/reverse_floyd.c
+++ PATTERNS/reverse_floyd.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,j,k=1;
+	int n,i,j,k;
 	printf("Enter the Number:");
 	scanf("%d",&n);
+	k=n*(n+1)/2;
 	for(i=1;i<=n;i++)
 	{
 		for(j=1;j<=i;j++)
 		{
-			printf(" %d",k);
-			k++;
+			printf("%d ",k);
+			k--;
 		}
 		printf("\n");
 	}
