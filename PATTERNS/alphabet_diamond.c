44106c9c3c52d16e92524b5d345696ede6ed3927
Tagore <tagore1066@gmail.com>
2026-02-27 17:32:09 +0530

    patterens

---
+++ PATTERNS/alphabet_diamond.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+	int i, j, n;
+	char ch='A';
+	printf("Enter number of rows: ");
+	scanf("%d", &n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("%c",ch+j-1);
+		}
+		for(j=i-1;j>=1;j--)
+		{
+			printf("%c",ch+j-1);
+		}
+		printf("\n");
+	}
+	for(i=n-1;i>=1;i--)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("%c",ch+j-1);
+		}
+		for(j=i-1;j>=1;j--)
+		{
+			printf("%c",ch+j-1);
+		}
+		printf("\n");
+	}
+	return 0;
+}
